d76df1ba82f28a99123e511139f2b1a5d894788f
Thomas <106164290+kobetoto@users.noreply.github.com>
2024-12-06 08:56:02 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -11,38 +11,35 @@
 /* ************************************************************************** */
 #include "get_next_line.h"
 
+
 char	*get_next_line(int fd)
 {
-	/*	variables	*/
-	static t_list	*stash;
+	static t_list	*lst;
 	char		*line;
-	int		readed;
 
-	/*	gestions des arguments	*/
-	if (fd < 0 || BUFFER_SIZE <= 0) //si le fichier on na pas le droit de le lire 
-		return (NULL);
-
-	readed = 1;
 	line = NULL;
-
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 
 
 	/*	steps	*/
-
-	// 1. read from fd and add to stash
-	read_and_stash(fd, &stash, &readed);
-	if(stash == NULL)
+	/* 1. read from fd and add to lst	*/
+	read_and_addlst(fd, &lst);
+	if(lst == NULL)
 		return (NULL);
-	// 2. extract from stash to line
-	extract_line(stash, &line);
-	// 3. clean stash
-	clean_stash(&stash);
+
+	/* 2. extract from stash to line	*/
+	extract_line(lst, &line);
+
+	/* 3. clean stash			*/
+	clean_lst(&lst);
 	if (line[0] == '\0')
 	{
-		free_stash(stash);
-		stash = NULL; 
+		free_lst(lst);
+		lst = NULL; 
 		free(line);
 		return (NULL);
 	}
 	return (line);
 }
+
