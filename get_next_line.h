a478576cc2abe77e6389d43b72dbc6c0f8a38b0e
Thomas <106164290+kobetoto@users.noreply.github.com>
2024-12-06 08:57:44 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -9,28 +9,32 @@
 /*   Updated: 2024/12/04 13:54:47 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 #ifndef BUFFER_SIZE
 #   define BUFFER_SIZE 5
 #endif
 
-typedef struct	s_list
+typedef struct s_list
 {
-	char		*content;
-	struct s_list	*next;
-}		t_list;
+    char *content;
+    struct s_list *next;
+} t_list;
+
 
 char	*get_next_line(int fd);
-int     found_newLine(t_list *stash);
-void    generate_line(char **line, t_list *stash);
-void    read_and_stash(int fd, t_list **stash, int *readed_ptr);
-void    add_to_stash(t_list **stash, char *buffer, size_t readed);
-void    extract_line(t_list *stash, char **line);
-void    clean_stash(t_list **stash);
-t_list  *ft_lst_get_last(t_list *stash);
-void    free_stash(t_list *stash);
+int     find_new_line(t_list *lst);
+void    generate_line(char **line, t_list *lst);
+void    read_and_addlst(int fd, t_list **lst);
+void    add_to_lst(t_list **lst, char *buffer, int readed);
+void    extract_line(t_list *lst, char **line);
+void    clean_lst(t_list **lst);
+t_list  *ft_lst_get_last(t_list *lst);
+void    free_lst(t_list *lst);
 int     ft_strlen(char *s);
+#endif
