01fca46a673b65c06bd2f4b83189f96fbdeb220f
Thomas <106164290+kobetoto@users.noreply.github.com>
2024-12-10 15:30:56 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,14 +6,12 @@
 /*   By: thodavid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 16:16:09 by thodavid          #+#    #+#             */
-/*   Updated: 2024/12/04 16:51:00 by thodavid         ###   ########.fr       */
+/*   Updated: 2024/12/10 15:18:10 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h> 
-
+#include "get_next_line.h" 
 
 /*			BOOL => if('\n') 1 else 0					*/
-
 int	find_new_line(t_list *lst)
 {
 	int	i;
@@ -35,8 +33,7 @@ int	find_new_line(t_list *lst)
 }
 
 /*              add the content of the buffer to the end of your stash                  */
-
-void    add_to_lst(t_list **lst, char *buffer, int readed)
+void    add_to_lst(t_list **lst, char *buffer, int char_)
 {
         int     i;
         t_list  *last;
@@ -47,10 +44,10 @@ void    add_to_lst(t_list **lst, char *buffer, int readed)
         if (!new_node)
                 return;
         new_node -> next = NULL;
-        new_node -> content = malloc(sizeof(char) * (readed +1));
+        new_node -> content = malloc(sizeof(char) * (char_ +1));
         if( new_node -> content == NULL)
                 return;
-        while (buffer[i] && i < readed)
+        while (buffer[i] && i < char_)
         {
                 new_node -> content[i] = buffer[i];
                 i++;
@@ -63,7 +60,18 @@ void    add_to_lst(t_list **lst, char *buffer, int readed)
         }
         last = ft_lst_get_last(*lst);
         last -> next = new_node;
+}
 
+
+/*              return a pointer to the last node                                       */
+t_list  *ft_lst_get_last(t_list *lst)
+{
+        t_list  *node;
+
+        node = lst;
+        while (node && node -> next)
+                node = node -> next;
+        return (node);
 }
 
 
@@ -94,29 +102,7 @@ void	generate_line(char **line, t_list *lst)
 		return ;
 } 
 
-
-
-
-
-
-/*              return a pointer to the last node                                       */
-
-t_list  *ft_lst_get_last(t_list *lst)
-{
-        t_list  *node;
-
-        node = lst;
-        while (node && node -> next)
-                node = node -> next;
-        return (node);
-}
-
-
-
-
-
-/*              free the entire stash  		                                     	*/
-
+/*              free the entire lst  		                                     	*/
 void	free_lst(t_list *lst)
 {
 	t_list	*current;
@@ -132,7 +118,7 @@ void	free_lst(t_list *lst)
 	}
 }
 
-
+/*		strlen		*/
 int	ft_strlen(char *s)
 {
 	if(!s)
